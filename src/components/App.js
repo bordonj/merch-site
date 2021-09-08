264743d6b7ad567228b1cff4ba8e99f683162f44
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-09-08 12:00:31 -0700

    import MerchControl to App.js
    Co-authored-by: giang184 <giang184@users.noreply.github.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navbar from './Navbar';
-// import SiteContainer from './SiteContainer';
-
+import MerchControl from './MerchControl';
 function App(){
   return (
     <React.Fragment>
