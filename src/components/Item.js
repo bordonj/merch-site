33a14fe1943fef6a9a53c91c284651d6ab5689e0
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-09-09 10:33:59 -0700

    clean files
    Co-authored-by: giang184 <giang184@users.noreply.github.com>

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
 
 function Item(props) {
+  const [itemCount, setItemCount] = useState(0)
+
+  function addOne () {
+    setItemCount(itemCount + 1)  
+  }
+
+
   return (
     <React.Fragment>
       <h3>{props.name}</h3>
       <p>{props.description}</p>
       <p>Left in stock: {props.quantity}</p>
-      <p>Left in stock: {itemCount}</p>
       <button onClick={addOne}>Restock by 1</button>
       <hr />
     </React.Fragment>
