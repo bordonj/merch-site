b2e4c859730fab6d91ec953d6385e2673b2bbea0
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-09-09 10:45:18 -0700

    delete addOne
    Co-authored-by: giang184 <giang184@users.noreply.github.com>

--- src/components/Merch.js
+++ src/components/Merch.js
@@ -1,20 +1,13 @@
-import React, {useState} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
 function Merch(props) {
-  const [MerchCount, setMerchCount] = useState(0)
-
-  function addOne () {
-    setMerchCount(MerchCount + 1)  
-  }
-
 
   return (
     <React.Fragment>
       <h3>{props.name}</h3>
       <p>{props.description}</p>
       <p>Left in stock: {props.quantity}</p>
-      <button onClick={addOne}>Restock by 1</button>
       <hr />
     </React.Fragment>
   )
