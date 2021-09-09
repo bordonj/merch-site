2403721391966c581f96d481f258ae60c860b1e8
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-09-09 11:43:32 -0700

    pass onClickingDelete and add to onClick button
    Co-authored-by: giang184 <giang184@users.noreply.github.com>

--- src/components/MerchDetail.js
+++ src/components/MerchDetail.js
@@ -2,21 +2,24 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 function MerchDetail(props){
-  const {merch} = props;
+  const {merch, onClickingDelete} = props;
 
   return (
     <React.Fragment>
-    <h1>Name: {merch.name}</h1>
-    <h3>Description: {merch.description}</h3>
-    <h3>Quantity: {merch.quantity}</h3>
-    <h3>ID: {merch.id}</h3>
-    <hr/>
-  </React.Fragment>
+      <h1>Name: {merch.name}</h1>
+      <h3>Description: {merch.description}</h3>
+      <h3>Quantity: {merch.quantity}</h3>
+      <h3>ID: {merch.id}</h3>
+      <button onClick={() => onClickingDelete(merch.id)}>Delete Merch
+      </button>
+      <hr/>
+    </React.Fragment>
   );
 }
 
 MerchDetail.propTypes = {
-  merch: PropTypes.object
+  merch: PropTypes.object,
+  onClickingDelete: PropTypes.func
 };
 
 export default MerchDetail;
