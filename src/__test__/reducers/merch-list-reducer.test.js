7497f7d3dae4887928a5535b6579d3466749c8fb
Andrew Giang <giang184@gmail.com>
2021-09-13 14:11:52 -0700

    pass second test
    Co-authored-by: professional-pigeon <professional-pigeon@users.noreply.github.com>
    Co-authored-by: bordonj <bordonj@users.noreply.github.com>

--- src/__test__/reducers/merch-list-reducer.test.js
+++ src/__test__/reducers/merch-list-reducer.test.js
@@ -2,8 +2,36 @@ import merchListReducer from '../../reducers/merch-list-reducer';
 
 describe('merchListReducer', () => {
 
+  let action;
+  const merchData = {
+    name: 'Goose Costume',
+    description: 'Fly around town, like a bird.',
+    quantity: 100,
+    id: 1
+  };
+
   test('Should return default state if no action type is recognized', () => {
     expect(merchListReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should successfully add new merch data to masterMerchList', () => {
+    const { name, description, quantity, id } = merchData;
+    action = {
+      type: 'ADD_MERCH',
+      name: name,
+      description: description,
+      quantity: quantity,
+      id: id
+    };
+
+    expect(merchListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        description: description,
+        quantity: quantity,
+        id: id
+      }
+    });
   });
 
 });
--- src/reducers/merch-list-reducer.js
+++ src/reducers/merch-list-reducer.js
@@ -1,3 +1,16 @@
-export default (state = {}, action) => { 
+export default (state = {}, action) => {
+  const { name, description, quantity, id } = action;
+  switch (action.type) {
+    case 'ADD_MERCH':
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          description: description,
+          quantity: quantity,
+          id: id
+        }
+      });
+  default:
     return state;
+  }
 };
